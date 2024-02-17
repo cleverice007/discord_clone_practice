4944436074471686c21a1f0e96b0e61a8fd16061
cleverice007 <cleverice90128@gmail.com>
2024-02-17 13:47:00 +0800

    Create primaryButton.js

---
+++ frontend/src/components/primaryButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const primaryButton = ({ label, additionalStyles, disabled, onClick }) => {
+  return (
+    <button
+      className={`text-white bg-blue-600 hover:bg-blue-700 font-medium text-base px-4 py-2 rounded transition duration-150 ease-in-out ${
+        disabled ? 'opacity-50 cursor-not-allowed' : ''
+      } ${additionalStyles}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default primaryButton;
