c6337b4375fd11f68841f0e225ff5faf7e8a399e
cleverice007 <cleverice90128@gmail.com>
2024-02-17 13:35:31 +0800

    Create authBox.js

---
+++ frontend/src/components/authBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const authBox = ({ children }) => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-indigo-600">
+      <div className="w-[700px] h-[400px] bg-gray-800 rounded-lg shadow-lg flex flex-col p-6">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default authBox;
