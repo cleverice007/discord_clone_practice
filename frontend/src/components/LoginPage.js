35f141a9d72953464e49e4f75b536fd4220d29d5
cleverice007 <cleverice90128@gmail.com>
2024-02-18 11:06:14 +0800

    create validate register ,login form in validator.js

---
+++ frontend/src/components/LoginPage.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+import AuthBox from "./AuthBox";
+import LoginPageFooter from "./LoginPageFooter";
+import LoginPageHeader from "./LoginPageHeader";
+import LoginPageInputs from "./LoginPageInputs";
