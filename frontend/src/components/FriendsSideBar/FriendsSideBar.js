9ba539226c34447bb04f810fb588b3cec6a97a38
cleverice007 <cleverice90128@gmail.com>
2024-02-20 10:47:22 +0800

    create FriendsTitle

--- frontend/src/components/FriendsSideBar/FriendsSideBar.js
+++ frontend/src/components/FriendsSideBar/FriendsSideBar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AddFriendButton from "./AddFriendButton";
+import FriendsTitle from "./FriendsTitle";
 
 
 const FriendsSideBar = () => {
   return (
     <div className="w-56 h-full flex flex-col items-center bg-gray-800">
       <AddFriendButton />
+      <FriendsTitle title="Private Messages" />
     </div>
   );
 };
---
+++ frontend/src/components/FriendsSideBar/FriendsTitle.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FriendsTitle = ({ title }) => {
+  return (
+    <div className="uppercase text-gray-500 text-sm mt-2.5">
+      {title}
+    </div>
+  );
+};
+
+export default FriendsTitle;
