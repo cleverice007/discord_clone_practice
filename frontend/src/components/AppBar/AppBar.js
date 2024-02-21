419856f29a16f62da05fa752a85e2e89fb177214
cleverice007 <cleverice90128@gmail.com>
2024-02-21 11:01:07 +0800

    修改appbar css, 去除白色空隙

--- frontend/src/components/AppBar/AppBar.js
+++ frontend/src/components/AppBar/AppBar.js
@@ -3,7 +3,7 @@ import DropdownMenu from './DropdownMenu';
 
 const AppBar = () => {
   return (
-    <div className="absolute right-0 top-0 h-12 border-b border-black bg-gray-900 w-[calc(100%-326px)] flex items-center justify-between px-4">
+    <div className="absolute right-0 top-0 h-12 border-b border-black bg-gray-900 w-[calc(100%-270px)] flex items-center justify-between px-4">
       <DropdownMenu />
     </div>
   );
