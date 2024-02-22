4b52fe0bdfb6231c2e12612c008bc95c3e013f15
cleverice007 <cleverice90128@gmail.com>
2024-02-22 12:19:59 +0800

    user schema 更新friends column

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   mail: { type: String, unique: true },
   username: { type: String },
   password: { type: String },
+  friends: [{ type: Schema.Types.Object, ref: "User" }],
 });
 
 const user = mongoose.model("user", userSchema);
