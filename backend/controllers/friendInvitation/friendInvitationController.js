14f0855662405e1242da144c0709754f4e601900
cleverice007 <cleverice90128@gmail.com>
2024-02-23 10:42:29 +0800

    create declineFriendInvitation  function

--- backend/controllers/friendInvitation/friendInvitationController.js
+++ backend/controllers/friendInvitation/friendInvitationController.js
@@ -103,4 +103,27 @@ const acceptFriendInvitation = async (req, res) => {
   }
 };
 
-export { inviteFriend, acceptFriendInvitation };
+const declineFriendInvitation = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const { userId } = req.user;
+
+    // remove that invitation from friend invitations collection
+    const invitationExists = await FriendInvitation.exists({ _id: id });
+
+    if (invitationExists) {
+      await FriendInvitation.findByIdAndDelete(id);
+    }
+
+    // update pending invitations
+    friendsUpdates.updateFriendsPendingInvitations(userId);
+
+    return res.status(200).send("Invitation succesfully rejected");
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Something went wrong please try again");
+  }
+};
+
+
+export { inviteFriend, acceptFriendInvitation, declineFriendInvitation};
