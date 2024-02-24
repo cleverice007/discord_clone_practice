7f7f2de5ffd2645ad6409007522cc6c1c109e29c
cleverice007 <cleverice90128@gmail.com>
2024-02-24 10:32:59 +0800

    Update socketConnection.js
    1. 監聽friends-invitations ,friends-list ,online-users socket event
    2. 獲取pending invitations , friends ,online users in frontend

--- frontend/src/realtimeCommunication/socketConnection.js
+++ frontend/src/realtimeCommunication/socketConnection.js
@@ -1,5 +1,7 @@
 import io from "socket.io-client";
+import { setPendingFriendsInvitations, setFriends, setOnlineUsers } from '../slices/friendSlice';
 let socket = null;
+
 
 export const connectWithSocketServer = (userDetails) => {
   const jwtToken = userDetails.token;
@@ -13,6 +15,21 @@ export const connectWithSocketServer = (userDetails) => {
   socket.on("connect", () => {
     console.log("succesfully connected with socket.io server");
     console.log(socket.id);
+  });
+
+  socket.on("friends-invitations", (data) => {
+    const { pendingInvitations } = data;
+    dispatch(setPendingFriendsInvitations(pendingInvitations));
+  });
+
+  socket.on("friends-list", (data) => {
+    const { friends } = data;
+    dispatch(setFriends(friends));
+  });
+
+  socket.on("online-users", (data) => {
+    const { onlineUsers } = data;
+    dispatch(setOnlineUsers(onlineUsers));
   });
 
 };
