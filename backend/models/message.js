b3722d23e8e5bfdcc91a5e3dde25543f3dffd73e
cleverice007 <cleverice90128@gmail.com>
2024-03-03 20:25:44 +0800

    create message schema

---
+++ backend/models/message.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  content: { type: String },
+  date: { type: Date },
+  type: { type: String },
+});
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
