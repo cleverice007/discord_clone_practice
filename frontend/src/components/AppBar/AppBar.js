f5f76d17dba94a0dbe857fd2ddecc6fa46ee3260
cleverice007 <cleverice90128@gmail.com>
2024-03-03 20:36:52 +0800

    import ChosenOptionLabel  into appbar

--- frontend/src/components/AppBar/AppBar.js
+++ frontend/src/components/AppBar/AppBar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import DropdownMenu from './DropdownMenu';
+import ChosenOptionLabel from "./ChosenOptionLabel";
 
 const AppBar = () => {
   return (
     <div className="absolute right-0 top-0 h-12 border-b border-black bg-gray-900 w-[calc(100%-270px)] flex items-center justify-between px-4">
+      <ChosenOptionLabel />
       <DropdownMenu />
     </div>
   );
