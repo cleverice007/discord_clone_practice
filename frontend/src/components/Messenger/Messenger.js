439e7c70b97aea60eaa3ce37c4a47e35e92ddd56
cleverice007 <cleverice90128@gmail.com>
2024-03-03 19:11:14 +0800

    create messenger 中的welcome message component

--- frontend/src/components/Messenger/Messenger.js
+++ frontend/src/components/Messenger/Messenger.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { useSelector } from 'react-redux';
+import WelcomeMessage from "./WelcomeMessage";
+import MessengerContent from "./MessengerContent";
 
 const Messenger = () => {
+  const chosenChatDetails = useSelector((state) => state.chat.chosenChatDetails);
+
   return (
-    <div className="flex-grow bg-gray-900 mt-12">
+    <div className="flex-grow bg-[#36393f] mt-[48px] flex">
+      {!chosenChatDetails ? (
+        <WelcomeMessage />
+      ) : (
+        <MessengerContent chosenChatDetails={chosenChatDetails} />
+      )}
     </div>
   );
 };
---
+++ frontend/src/components/Messenger/WelcomeMessage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const WelcomeMessage = () => {
+  return (
+    <div className="flex-grow h-full flex items-center justify-center">
+      <p className="text-white text-lg">
+        To start chatting - choose conversation
+      </p>
+    </div>
+  );
+};
+
+export default WelcomeMessage;
