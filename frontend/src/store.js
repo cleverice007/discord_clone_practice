5452439a31d98d926a32d17eda8c3c70e9b39246
cleverice007 <cleverice90128@gmail.com>
2024-03-03 18:45:57 +0800

    create chat reducer and actions to use Redux Toolkit's createSlice
    create  setChosenChatDetails, setMessages function

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/apiSlice'; 
 import authReducer from './slices/authSlice';
 import friendReducer from './slices/friendSlice';
+import chatReducer from './slices/chatSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     friends: friendReducer,
+    chat: chatReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
