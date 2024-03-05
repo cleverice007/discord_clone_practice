b7eb547185ad13dd712cee5dea364cb2ad61ad0e
cleverice007 <cleverice90128@gmail.com>
2024-03-05 10:51:29 +0800

    debug

--- frontend/src/components/FriendsSideBar/PendingInvitationsList/PendingInvitationList.js
+++ frontend/src/components/FriendsSideBar/PendingInvitationsList/PendingInvitationList.js
@@ -6,15 +6,15 @@ import { useSelector } from 'react-redux';
 const PendingInvitationsList = () => {
   const pendingFriendsInvitations = useSelector((state) => state.friends.pendingFriendsInvitations);
   
-
+  
   return (
     <div className="w-full h-1/5 flex flex-col items-center overflow-auto">
       {pendingFriendsInvitations.map((invitation) => (
         <PendingInvitationsListItem
           key={invitation._id}
           id={invitation._id}
-          username={invitation.senderId.username}
-          mail={invitation.senderId.mail}
+          username={invitation.senderId?.username}
+          mail={invitation.senderId?.mail}
         />
       ))}
     </div>
