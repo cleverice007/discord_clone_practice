68bc428f7a45bb985a8836661bb515e8a9f2ef8f
cleverice007 <cleverice90128@gmail.com>
2024-03-05 14:01:19 +0800

    create updateChatHistory to emit direct-chat-history event
    傳遞message 給在線上的participants

---
+++ backend/socketHandlers/updates/chat.js
@@ -0,0 +1,48 @@
+import Conversation from '../../../models/Conversation';
+import getActiveConnections from '../../../serverStore';
+
+
+const updateChatHistory = async (
+    conversationId,
+    toSpecifiedSocketId = null
+  ) => {
+    const conversation = await Conversation.findById(conversationId).populate({
+      path: "messages",
+      model: "Message",
+      populate: {
+        path: "author",
+        model: "User",
+        select: "username _id",
+      },
+    });
+  
+    if (conversation) {
+      const io = serverStore.getSocketServerInstance();
+  
+      if (toSpecifiedSocketId) {
+        // initial update of chat history
+        return io.to(toSpecifiedSocketId).emit("direct-chat-history", {
+          messages: conversation.messages,
+          participants: conversation.participants,
+        });
+      }
+  
+      // check if users of this conversation are online
+      // if yes emit to them update of messages
+  
+      conversation.participants.forEach((userId) => {
+        const activeConnections = getActiveConnections(
+          userId.toString()
+        );
+  
+        activeConnections.forEach((socketId) => {
+          io.to(socketId).emit("direct-chat-history", {
+            messages: conversation.messages,
+            participants: conversation.participants,
+          });
+        });
+      });
+    }
+  };
+
+  export { updateChatHistory}
