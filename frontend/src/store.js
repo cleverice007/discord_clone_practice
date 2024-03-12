fd074c995d0cfa89634c3e05621782322483b301
cleverice007 <cleverice90128@gmail.com>
2024-03-12 11:58:44 +0800

    import room reduce into store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,12 +3,14 @@ import { apiSlice } from './slices/apiSlice';
 import authReducer from './slices/authSlice';
 import friendReducer from './slices/friendSlice';
 import chatReducer from './slices/chatSlice';
+import roomRudecer from './slices/roomSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     friends: friendReducer,
     chat: chatReducer,
+    room: roomRudecer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
