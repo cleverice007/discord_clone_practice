46ffda409fc386ec09582f269617d0693a5d149b
cleverice007 <cleverice90128@gmail.com>
2024-03-12 12:30:24 +0800

    create updateRoom function 將activeRooms 用socket 傳回前端

---
+++ backend/socketHandlers/updates/room.js
@@ -0,0 +1,19 @@
+
+import { getSocketServerInstance, getActiveRooms } from "../../serverStore";
+
+const updateRooms = (toSpecifiedSocketId = null) => {
+    const io = getSocketServerInstance();
+    const activeRooms = getActiveRooms();
+  
+    if (toSpecifiedSocketId) {
+      io.to(toSpecifiedSocketId).emit("active-rooms", {
+        activeRooms,
+      });
+    } else {
+      io.emit("active-rooms", {
+        activeRooms,
+      });
+    }
+  };
+
+export { updateRooms };
