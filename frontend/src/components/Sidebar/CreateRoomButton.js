e33a34cf4c2781438a27ad9268bccfcec3b16fcf
cleverice007 <cleverice90128@gmail.com>
2024-03-12 10:25:50 +0800

    create createRoomButton

---
+++ frontend/src/components/Sidebar/CreateRoomButton.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const CreateRoomButton = ({ isUserInRoom }) => {
+  const createNewRoomHandler = () => {
+  };
+
+  return (
+    <button
+      disabled={isUserInRoom}
+      onClick={createNewRoomHandler}
+      className={`w-12 h-12 rounded-2xl m-0 p-0 text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none ${isUserInRoom ? 'opacity-50 cursor-not-allowed' : ''}`}
+      style={{ marginTop: "10px" }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6 m-auto" 
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M12 4v16m8-8H4"
+        />
+      </svg>
+    </button>
+  );
+};
+
+export default CreateRoomButton;
--- frontend/src/components/Sidebar/SideBar.js
+++ frontend/src/components/Sidebar/SideBar.js
@@ -1,10 +1,13 @@
 import React from "react";
 import MainPageButton from "./MainPageButton";
+import CreateRoomButton from "./CreateRoomButton";
 
 const SideBar = () => {
+  const isUserInRoom = false;
   return (
     <div className="w-18 h-full flex flex-col items-center bg-[#202225]">
       <MainPageButton />
+      <CreateRoomButton isUserInRoom={isUserInRoom} />
     </div>
   );
 };
