ef3cfc989938ac63066d4137e6351fb30490190e
cleverice007 <cleverice90128@gmail.com>
2024-03-12 10:38:23 +0800

    Implement room reducer for Redux to manage room states
    Key features implemented in this reducer include:
    - Opening and closing rooms, which updates the state to reflect whether the user is in a room or has created a room.
    - Setting the details of the current room, including room ID and other relevant information.
    - Managing the list of active rooms to update the UI accordingly.
    - Handling local and remote media streams to support real-time communication features.
    - Toggling audio-only mode and screen sharing, allowing users to join with only audio or share their screen during a call.

--- frontend/src/components/Sidebar/SideBar.js
+++ frontend/src/components/Sidebar/SideBar.js
@@ -1,9 +1,12 @@
 import React from "react";
 import MainPageButton from "./MainPageButton";
 import CreateRoomButton from "./CreateRoomButton";
+import { useSelector } from 'react-redux';
+
 
 const SideBar = () => {
-  const isUserInRoom = false;
+  const isUserInRoom = useSelector((state) => state.room.isUserInRoom);
+  
   return (
     <div className="w-18 h-full flex flex-col items-center bg-[#202225]">
       <MainPageButton />
---
+++ frontend/src/slices/roomSlice.js
@@ -0,0 +1,60 @@
+import { createSlice } from '@reduxjs/toolkit';
+const initialState = {
+    isUserInRoom: false,
+    isUserRoomCreator: false,
+    roomDetails: null,
+    activeRooms: [],
+    localStream: null,
+    remoteStreams: [],
+    audioOnly: false,
+    screenSharingStream: null,
+    isScreenSharingActive: false,
+    isUserJoinedWithOnlyAudio: false,
+  };
+
+  const roomSlice = createSlice({
+    name: 'room', 
+    initialState, 
+    reducers: { 
+      setOpenRoom(state, action) {
+        const { isUserRoomCreator, isUserInRoom } = action.payload;
+        state.isUserInRoom = isUserInRoom;
+        state.isUserRoomCreator = isUserRoomCreator;
+      },
+      setRoomDetails(state, action) {
+        state.roomDetails = action.payload.roomDetails;
+      },
+      setActiveRooms(state, action) {
+        state.activeRooms = action.payload.activeRooms;
+      },
+      setLocalStream(state, action) {
+        state.localStream = action.payload.localStream;
+      },
+      setAudioOnly(state, action) {
+        state.audioOnly = action.payload.audioOnly;
+      },
+      setRemoteStreams(state, action) {
+        state.remoteStreams = action.payload.remoteStreams;
+      },
+      setScreenSharingStream(state, action) {
+        state.screenSharingStream = action.payload.screenSharingStream;
+        state.isScreenSharingActive = action.payload.isScreenSharingActive;
+      },
+      setIsUserJoinedOnlyWithAudio(state, action) {
+        state.isUserJoinedWithOnlyAudio = action.payload.isUserJoinedWithOnlyAudio;
+      },
+    },
+  });
+  
+  export default roomSlice.reducer;
+  
+  export const {
+    setOpenRoom,
+    setRoomDetails,
+    setActiveRooms,
+    setLocalStream,
+    setAudioOnly,
+    setRemoteStreams,
+    setScreenSharingStream,
+    setIsUserJoinedOnlyWithAudio,
+  } = roomSlice.actions;
