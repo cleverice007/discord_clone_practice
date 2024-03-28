5309403f7388b96b610c09f38e9ad7c66f000500
cleverice007 <cleverice90128@gmail.com>
2024-03-28 10:45:36 +0800

    debug 修改送入redux 的screenshare stream 方式

--- frontend/src/components/Room/RoomButtons/ScreenShareButton.js
+++ frontend/src/components/Room/RoomButtons/ScreenShareButton.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDesktop, faStopCircle } from "@fortawesome/free-solid-svg-icons";
-import * as webRTCHandler from "../../../realtimeCommunication/webRTCHandler";
+import  {switchOutgoingTracks} from "../../../realtimeCommunication/webRTCHandler";
+import { setScreenSharingStream } from '../../../slices/roomSlice';
+import { useSelector, useDispatch } from 'react-redux';
+
 
 const constraints = {
   audio: false,
@@ -11,11 +14,26 @@ const constraints = {
 const ScreenShareButton = ({
   localStream,
   screenSharingStream,
-  setScreenSharingStream,
-  isScreenSharingActive,
 }) => {
-  const handleScreenShareToggle = async () => {
+  const dispatch = useDispatch();
+  const isScreenSharingActive = useSelector((state) => state.room.isScreenSharingActive);
 
+  const handleScreenShareToggle = async () => {
+    if (!isScreenSharingActive) {
+      let stream = null;
+      try {
+        stream = await navigator.mediaDevices.getDisplayMedia(constraints);
+        console.log("Screen sharing started successfully", stream);
+        dispatch(setScreenSharingStream({ screenSharingStream: stream }));
+      } catch (err) {
+        console.log("Error occurred when trying to get access to screen share stream");
+      }
+    } else {
+      console.log("Stopping screen sharing", screenSharingStream); 
+      switchOutgoingTracks(localStream);
+      screenSharingStream.getTracks().forEach((t) => t.stop());
+      dispatch(setScreenSharingStream({ screenSharingStream: null }));
+    }
   };
 
   return (
--- frontend/src/components/Room/VideosContainer.js
+++ frontend/src/components/Room/VideosContainer.js
@@ -7,7 +7,7 @@ const VideosContainer = () => {
   const localStream = useSelector((state) => state.room.localStream);
   const remoteStreams = useSelector((state) => state.room.remoteStreams);
   const screenSharingStream = useSelector((state) => state.room.screenSharingStream);
-
+console.log('screenSharingStream',screenSharingStream)
 
   return (
     <div className="h-5/6 w-full flex flex-wrap">
